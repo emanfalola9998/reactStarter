d5feb62ea45be58dcde7c31838a2614a7e6e47d0
emanfalola9998 <emmanuelfalola9998@gmail.com>
2022-03-04 11:50:32 +0000

    ran prettier

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,15 +1,17 @@
-function PageHeader(): JSX.Element{
-    return(
-        <>
-            <h1>These are my favourite places</h1>
-            <p><i>I hope you enjoy them as much as I did making this website</i></p>
-            {/* <button type="button" onclick="alert('Updated!')">update</button> */}
-            <nav>
-                <a href="https://www.britannica.com/topic/flag-of-Ireland">Ireland</a>|
-                <a href="https://www.britannica.com/topic/flag-of-Spain">Spain</a>|
-                <a href="https://www.britannica.com/topic/flag-of-Malta">Malta</a>
-            </nav>
-        </>
-    );   
+function PageHeader(): JSX.Element {
+  return (
+    <>
+      <h1>These are my favourite places</h1>
+      <p>
+        <i>I hope you enjoy them as much as I did making this website</i>
+      </p>
+      {/* <button type="button" onclick="alert('Updated!')">update</button> */}
+      <nav>
+        <a href="https://www.britannica.com/topic/flag-of-Ireland">Ireland</a>|
+        <a href="https://www.britannica.com/topic/flag-of-Spain">Spain</a>|
+        <a href="https://www.britannica.com/topic/flag-of-Malta">Malta</a>
+      </nav>
+    </>
+  );
 }
 export default PageHeader;
--- src/components/Places.tsx
+++ src/components/Places.tsx
@@ -1,40 +1,38 @@
-
-interface PlacesEntryProps{
-    title: number
-    place: string
-    countryName: string
-    mainImage: string
-    descrptImage: string
-    width: number
-    height: number
-    locationURL: string
-    location: string
-    info: string
+interface PlacesEntryProps {
+  title: number;
+  place: string;
+  countryName: string;
+  mainImage: string;
+  descrptImage: string;
+  width: number;
+  height: number;
+  locationURL: string;
+  location: string;
+  info: string;
 }
 
-
-function Places(Props:PlacesEntryProps): JSX.Element{
-    return(
-        <>
-            <h2> {Props.title}</h2>
-            <p><b>I visited {Props.place}</b></p>
-            <p>Located in {Props.countryName}</p>
-            <p>Notable places to visit in {Props.place}</p>
-            <a href={Props.locationURL}>{Props.location}</a>
-            <br></br>
-            <img src={Props.mainImage} alt={Props.descrptImage} width={Props.width} height={Props.height}/>
-            <p>{Props.info}</p>
-        </>
-    )
+function Places(Props: PlacesEntryProps): JSX.Element {
+  return (
+    <>
+      <h2> {Props.title}</h2>
+      <p>
+        <b>I visited {Props.place}</b>
+      </p>
+      <p>Located in {Props.countryName}</p>
+      <p>Notable places to visit in {Props.place}</p>
+      <a href={Props.locationURL}>{Props.location}</a>
+      <br></br>
+      <img
+        src={Props.mainImage}
+        alt={Props.descrptImage}
+        width={Props.width}
+        height={Props.height}
+      />
+      <p>{Props.info}</p>
+    </>
+  );
 }
-export default Places
-
-
-
-
-
-
-
+export default Places;
 
 // a title (text)
 // a place name (text)
@@ -46,7 +44,7 @@ export default Places
 // interface NavItemProps {
 //     label: string;
 //   }
-  
+
 //   function NavItem(props: NavItemProps): JSX.Element {
 //     return (
 //       <>
@@ -55,5 +53,5 @@ export default Places
 //       </>
 //     );
 //   }
-  
+
 //   export default NavItem;
