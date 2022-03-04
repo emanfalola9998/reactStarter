74f56ae71c0611910528d16489c63766750e83da
emanfalola9998 <emmanuelfalola9998@gmail.com>
2022-03-04 15:02:54 +0000

    adding friends links

---
+++ src/components/PageFooter.tsx
@@ -0,0 +1,19 @@
+function PageFooter(): JSX.Element {
+    return (
+      <>
+        <h2>These are my friends sites; go on have a look! </h2>
+        
+        {/* <button type="button" onclick="alert('Updated!')">update</button> */}
+        <nav>
+          <a href="https://hardcore-northcutt-fafb4e.netlify.app/">Laura</a>|
+          <a href="https://temis-favourite-places.netlify.app/">Temi</a>|
+          {/* <a href="https://www.britannica.com/topic/flag-of-Malta">Malta</a> */}
+        </nav>
+        <p>
+          <i>Thanks for visiting</i>
+        </p>
+      </>
+    );
+  }
+  export default PageFooter;
+  
